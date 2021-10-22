98cd6e59a3f808f9a2b8c3afbe7d7f1de09e06a3
gonza <gonza56d@gmail.com>
2021-10-21 21:27:10 -0300

    Exceptions with colors

--- sql_formatter.py
+++ sql_formatter.py
@@ -2,11 +2,14 @@
 
 gonzalomanuel.garcia@equifax.com / gonzalo.garcia01@globant.com"""
 
+from colorama import Fore
 from sys import argv
 
 
 class CommandException(Exception):
-    pass
+    
+    def __init__(self, message) -> None:
+        super().__init__(Fore.RED + '\n\nError de comando: ' + message)
 
 
 def is_a_command(line: str) -> bool:
@@ -56,12 +59,15 @@ def format(file: str) -> str:
 
 
 if __name__ == '__main__':
+    file_name = ''
     try:
-        sql_name = argv[1]
-        formatted_text = format(sql_name)
-        with open(f"{sql_name.replace('.sql', '')}_formatted.sql", 'w') as formatted_sql:
+        file_name = argv[1]
+        formatted_text = format(file_name)
+        with open(f"{file_name.replace('.sql', '')}_formatted.sql", 'w') as formatted_sql:
             formatted_sql.write(formatted_text)
     except IndexError:
-        raise CommandException('\n\nDebe indicar el archivo SQL a formatear.\n'
+        raise CommandException('Debe indicar el archivo SQL a formatear.\n'
             + 'Ejemplo: sql_formatter.py my_query.sql')
+    except FileNotFoundError:
+        raise CommandException(f'No se pudo encontrar el archivo "{file_name}".')
     
